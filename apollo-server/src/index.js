e73f53dcb6f3d918c5bcaee3ba7c1478d8b265c3
Jacob Jedryszek <jj09@fb.com>
2022-12-27 17:45:18 -0800

    Setup Apollo Server

---
+++ apollo-server/src/index.js
@@ -0,0 +1,24 @@
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+import gql from 'graphql-tag';
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+const resolvers = {
+    Query: {
+        hello: () => 'Hello from Apollo Server',
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+const { url } = await startStandaloneServer(server, {
+    listen: {
+        port: 4000,
+    },
+});
+
+console.info(`🚀 Server ready at ${url}`);
