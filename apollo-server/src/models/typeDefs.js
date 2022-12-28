633f30fe25b232a1c034b63aa8f191f134af7806
Jacob Jedryszek <jj09@fb.com>
2022-12-27 18:23:32 -0800

    Create Book model and query books from MongoDB

--- apollo-server/src/models/typeDefs.js
+++ apollo-server/src/models/typeDefs.js
@@ -3,5 +3,11 @@ import gql from 'graphql-tag';
 export const typeDefs = gql`
     type Query {
         hello(name: String): String
+        books: [Book]
+    }
+    type Book {
+        id: ID,
+        title: String,
+        year: Int,
     }
 `;
--- apollo-server/src/resolvers.js
+++ apollo-server/src/resolvers.js
@@ -1,5 +1,8 @@
+import {Book} from './models/Book.js';
+
 export const resolvers = {
     Query: {
         hello: (_, {name}) => `Hello ${name}`,
+        books: async () => await Book.find({}),
     },
 };
