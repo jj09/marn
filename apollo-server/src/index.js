2ece79747630c30b16d6c9fdeb12a203d19beab2
Jacob Jedryszek <jj09@fb.com>
2022-12-27 18:17:02 -0800

    Connect to MongoDB from node with mongoose

--- apollo-server/src/index.js
+++ apollo-server/src/index.js
@@ -2,6 +2,11 @@ import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from '@apollo/server/standalone';
 import {typeDefs} from './models/typeDefs.js';
 import {resolvers} from './resolvers.js';
+import mongoose from 'mongoose';
+
+const db = await mongoose.connect('mongodb://localhost:27017/marn');
+
+console.info('📚 Connected to db', db?.connections[0]?._connectionString);
 
 const server = new ApolloServer({typeDefs, resolvers});
 
