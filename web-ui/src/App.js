137324c27a8e0f0fd563a45c3125f556983c72cd
Jacob Jedryszek <jj09@fb.com>
2022-12-27 18:38:56 -0800

    Add React Router

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,6 +1,7 @@
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import Hello from './components/Hello';
 import Books from './components/Books';
+import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -10,8 +11,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Hello />
-      <Books />
+      <BrowserRouter>
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto flex-row">
+            <Link to="/" className="nav-link mr-2">Home</Link>
+            <Link to="/books" className="nav-link mr-2">Books</Link>
+          </div>
+        </nav>
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<Hello />} />
+            <Route path="/books" element={<Books />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
